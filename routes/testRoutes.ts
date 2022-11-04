99daf9f327af0fa7a2cbd47ebfb0f6147cbdcad1
Candace <candacecsy@gmail.com>
2022-11-04 12:31:13 -0400

    diabetes update test routes

--- routes/testRoutes.ts
+++ routes/testRoutes.ts
@@ -5,6 +5,7 @@ import { logger } from "../util/logger";
 export const testRoutes = express.Router();
 
 testRoutes.post('/heart', testHeartDisease)
+testRoutes.post('/diabetes', testDiabetes)
 
 async function testHeartDisease(req: Request, res: Response) {
     try {
@@ -25,8 +26,6 @@ async function testHeartDisease(req: Request, res: Response) {
 
         const result = await fetchRes.json()
 
-        
-
         res.json({
             status: true,
             result: result
@@ -36,6 +35,38 @@ async function testHeartDisease(req: Request, res: Response) {
         logger.error(e)
         res.status(400).json({
             msg: 'ERR005: Something Went Wrong When Sending Test Data'
+        })
+    }
+}
+
+async function testDiabetes(req: Request, res: Response) {
+    try{
+        logger.debug('before sending test data')
+        const testData = req.body
+
+        const fetchRes = await fetch('http://localhost:8000/index/test/diabetes', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify([testData])
+        })
+        
+        if (fetchRes.status >= 400) {
+            throw new Error('"Bad response from test server"')
+        }
+
+        const result = await fetchRes.json()
+
+        res.json({
+            status: true,
+            result: result
+        });
+
+    } catch (e) {
+        logger.error(e)
+        res.status(400).json({
+            msg: 'ERR006: Error of sending test data.'
         })
     }
 }
