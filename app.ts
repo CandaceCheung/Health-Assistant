d228ce32303633cb48bc0150b1114bb330ec1654
Yip Chak Ming, Frankie <cmyip6@gmail.com>
2022-11-10 14:21:29 -0500

    for deployment testing

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ import path from 'path';
 import dontenv from 'dotenv';
 import Knex from 'knex';
 import { infoRoutes } from './routes/infoRoutes';
-import { testRoutes } from './routes/testRoutes';
+import { testRoutes2 } from './routes/testRoutes2';
 
 dontenv.config();
 
@@ -23,7 +23,7 @@ const sessionMiddleware = expressSession({
 
 app.use(cookieParser(), express.json(), sessionMiddleware, express.static('public'));
 app.use('/info', infoRoutes);
-app.use('/test', testRoutes)
+app.use('/test', testRoutes2)
 
 app.use((req, res) => {
 	res.status(404).sendFile(path.resolve('./public/404.html'));
