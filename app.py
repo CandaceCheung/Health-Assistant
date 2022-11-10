822d3d5b73ce99b1ff2906dbaaeaec16b6379f4d
Candace <candacecsy@gmail.com>
2022-11-10 11:24:57 -0500

    sanic update process

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def callStrokeModel(request):
     return json({"data":results})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000,  single_process=True)
